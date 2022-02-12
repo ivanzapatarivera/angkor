a50abd2fa85326a129de49d86736d5f198a890b8
Ivan Zapata <falenyo@gmail.com>
2022-02-12 16:54:04 -0600

    Installed dependencies

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 const cb = "client/build"
-
 app.use(express.static(path.resolve(__dirname, cb)));
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, cb, "index.html"));
