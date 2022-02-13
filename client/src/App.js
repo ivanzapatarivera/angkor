3221911d9583dfb99e15e84350cd1199cf90d440
Ivan Zapata <falenyo@gmail.com>
2022-02-12 20:08:21 -0600

    Changed boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Route, Link, Routes } from "react-router-dom";
+
+const Main = () => <h1>Main!</h1>;
+const Elsewhere = () => <h1>Elsewhere!</h1>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Main</Link>
+            </li>
+            <li>
+              <Link to="/else">Else</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" exact component={Main} />
+          <Route path="/else/" component={Elsewhere} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
