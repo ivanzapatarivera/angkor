c437128ae96fa7aed248995c435eae277e8f0f15
Ivan Zapata <falenyo@gmail.com>
2022-02-12 20:14:11 -0600

    Changed app

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,7 @@
 import React from "react";
-import { Router, Route, Link, Routes } from "react-router-dom";
-
-const Main = () => <h1>Main!</h1>;
-const Elsewhere = () => <h1>Elsewhere!</h1>;
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Main</Link>
-            </li>
-            <li>
-              <Link to="/else">Else</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" exact component={Main} />
-          <Route path="/else/" component={Elsewhere} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <div>Hey, this is my app</div>;
 }
-
 
 export default App;
