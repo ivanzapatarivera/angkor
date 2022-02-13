21be72febbe7068fb113711e3f6fcedd1b891a77
Ivan Zapata <falenyo@gmail.com>
2022-02-13 12:56:59 -0600

    changes to build

--- client/src/Adelphi/Backgrounds/getDefaultBackgroundColor.js
+++
@@ -1,8 +0,0 @@
-let defaultBackgroundColor = "hsl(240, 100%, 3%)"
-
-let setBackgroundColor = (element) => {
-    console.log(element)
-    element.style.backgroundColor = defaultBackgroundColor;
-}
-
-export default setBackgroundColor;
