270ed39a7521d9ab55dc5e65f3b62e0ac5c3958b
Ivan J. Zapata-Rivera <ivan.zapata.rivera@gmail.com>
2022-12-23 15:50:41 -0400

    Blanked the document

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from "react";
-import { setElementStyleProperties } from "./Adelphi/Backgrounds/setDefaultStyles";
-import { listOfAppElements } from "./Railway/getAppElements";
 
 class App extends Component {
-  componentDidMount = () => {
-    this.getDefaultStyles();
-  };
-
-  getDefaultStyles = () => {
-    setElementStyleProperties(listOfAppElements);
-  };
-
+  
   render() {
     return (
       <div>
-        <div id="welcome">Hello, welcome to my app.</div>
-        <div id="message">Let's take a journey together</div>
+        
       </div>
     );
   }
