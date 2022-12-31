80e9b06565ce7a6b6c1011a14ed78882ef1e74b3
Ivan J. Zapata-Rivera <ivan.zapata.rivera@gmail.com>
2022-12-30 23:18:19 -0400

    Added map method in JSX line 43 to 45

--- client/src/setWelcomeLetter/letter.js
+++ client/src/setWelcomeLetter/letter.js
@@ -9,12 +9,13 @@ export default class Letter extends Component {
 
   returnIntroduction = () => {
     let introduction =
-      "My name is Ivan and thanks for stopping by. Since a young age, I’ve enjoyed traveling across five continents. It all started after joining friends on an adventure through South and Southeast Asia. I had such a wonderful experience that I decided to give it a go and continue doing what I enjoy the most.";
+      "My name is Ivan and thanks for stopping by.";
     return introduction;
   };
 
   returnText = () => {
     let textArr = [
+      "Since a young age, I’ve enjoyed traveling across five continents. It all started after joining friends on an adventure through South and Southeast Asia. I had such a wonderful experience that I decided to give it a go and continue doing what I enjoy the most.",
       "My passion grew as I visited Europe where I fell in love with its people, history and architecture. I also went on a few road trips through North America where I’ve seen one of a kind nature and felt the warmth of its people across Canada, United States, and Mexico. I’ve been to South America to visit friends, taste its gastronomy and dance to its music. ",
       "Although travel may sound like lots of fun, it’s an experience that becomes more incredible when it can be shared with friends from all Around the World. For a traveler, it does not only make each experience unique and cool, it also makes the world much smaller and intimate.",
       "So, I open the doors of my travel box and welcome you to join me through this journey.",
