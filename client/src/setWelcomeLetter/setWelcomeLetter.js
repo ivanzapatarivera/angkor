2e329268f5f0a83d9bd8e0a82703f7c419d35ba5
Ivan J. Zapata-Rivera <ivan.zapata.rivera@gmail.com>
2023-01-05 14:09:09 -0400

    Added Suspense and Lazy

--- client/src/setWelcomeLetter/setWelcomeLetter.js
+++ client/src/setWelcomeLetter/setWelcomeLetter.js
@@ -1,12 +1,15 @@
-import { Component } from "react";
-import Letter from "./letter";
+import React, { Component, Suspense } from "react";
 import "./style.css";
+
+const Letter = React.lazy(() => import("./letter"));
 
 export default class SetWelcomeLetter extends Component {
   render() {
     return (
       <div className="letter">
-        <Letter />
+        <Suspense fallback={<div>Loading</div>}>
+          <Letter />
+        </Suspense>
       </div>
     );
   }
